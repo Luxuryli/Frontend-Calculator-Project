32db15f0b04088c2eda4d3f3e528b134689987b3
Luxuryli <olakutope4@gmail.com>
2024-05-09 17:44:12 +0100

    2nd Commit

---
+++ main.js
@@ -0,0 +1,23 @@
+function appendToOutput(value) {
+    document.getElementById("output").value += value;
+}
+
+function clearOutput() {
+    document.getElementById("output").value = "";
+}
+
+function calculate() {
+    var expression = document.getElementById("output").value;
+    var result;
+
+    try {
+        result = eval(expression);
+        if (isNaN(result) || !isFinite(result)) {
+            throw "Invalid expression";
+        }
+    } catch (error) {
+        result = "Error";
+    }
+
+    document.getElementById("output").value = result;
+}

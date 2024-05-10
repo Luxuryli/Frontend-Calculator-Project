141b4bda26e4f4000d64c50538ace24772f34cb1
Luxuryli <olakutope4@gmail.com>
2024-05-10 01:19:40 +0100

    Edit JS for keyboard

--- main.js
+++ main.js
@@ -17,3 +17,27 @@ function calculate() {
 
     document.getElementById("output").value = result;
 }
+
+    const output = document.getElementById('output');
+    
+    document.addEventListener('keydown' ,  (event)  => {
+        console.log(event)
+        if(event.key == "0") {output.value += event.key;}
+        if(event.key == "1") {output.value += event.key;}       
+         if(event.key == "2") {output.value += event.key;}       
+         if(event.key == "3") {output.value += event.key;}       
+         if(event.key == "4") {output.value += event.key;}        
+         if(event.key == "5") {output.value += event.key;}        
+         if(event.key == "6") {output.value += event.key;}        
+         if(event.key == "7") {output.value += event.key;}        
+         if(event.key == "8") {output.value += event.key;}
+        if(event.key == "9") {output.value += event.key;}
+        if(event.key == ".") {output.value += event.key;}
+        if(event.key == "Backspace") {output.value = output.value.slice(0,-1);}
+        if(event.key == "+") {output.value += event.key;}
+        if(event.key == "-") {output.value += event.key;}      
+        if(event.key == "/") {output.value += event.key;}        
+        if(event.key == "*") {output.value += event.key;}
+        if(event.key == "=") {output.value = calculate(output.value);}
+
+    })
